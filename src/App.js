8c02284c6731f0df104ef6aa295be12f871f95dc
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-18 00:58:28 +0530

    TextUtils So Far

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import TechBar from './components/TechBar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TechBar from "./components/TechBar";
 
 function App() {
   return (
     <>
-    
-<Navbar  home="Home2"/>
-<div className="container">
-
-<TechBar head="Heading"/>
-</div>
+      <Navbar title="TextConvertor" home="Home" />
+      <div className="container">
+        <TechBar head="Text to Upper and Lower Case" />
+      </div>
     </>
   );
 }
